0491798c28c5eac02d0f793a1ec7633409398e3a
a 浩 <oaleo@vip.qq.com>
2024-01-03 07:46:19 +0000

    替换 欧帝影院.js

--- js/欧帝影院.js
+++ js/欧帝影院.js
@@ -23,3 +23,4 @@ var rule={
     },
     class_parse: '.navbar-items li:gt(1):lt(7);a&&title;a&&href;.*/(.*?).html',
     lazy:"js:var html=JSON.parse(request(input).match(/r player_.*?=(.*?)</)[1]);var url=html.url;if(html.encrypt=='1'){url=unescape(url)}else if(html.encrypt=='2'){url=unescape(base64Decode(url))}if(/m3u8|mp4/.test(url)){input=url}else{input}",
+}
